03d0dc1f7607ed2a8573c9e57beb81f809a3ad94
Maarten De Vleeschouwer <maarten.devleeschouwer@gmail.com>
2015-06-01 19:53:48 +0200

    V1.1 Klasse Visitor

---
+++ Ado.netProefexamen/BO/Visitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.netProefexamen.BO
+{
+    public class Visitor
+    {
+        public int VisitorID { get; set; }
+        public int EventiD { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+
+        public bool Notebook { get; set; }
+        public int LunchID { get; set; }
+        public DateTime SubscribeDate { get; set; } 
+
+
+    }
+}
