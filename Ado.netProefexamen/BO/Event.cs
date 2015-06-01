72ac2afd7e941adc552935c5f94052ac018ab796
Maarten De Vleeschouwer <maarten.devleeschouwer@gmail.com>
2015-06-01 19:56:07 +0200

    V1.2 EventClass

---
+++ Ado.netProefexamen/BO/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.netProefexamen.BO
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        public string Omschrijving { get; set; }
+
+    }
+}
--- Ado.netProefexamen/BO/Visitor.cs
+++ Ado.netProefexamen/BO/Visitor.cs
@@ -14,7 +14,6 @@ namespace Ado.netProefexamen.BO
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Email { get; set; }
-
         public bool Notebook { get; set; }
         public int LunchID { get; set; }
         public DateTime SubscribeDate { get; set; } 
